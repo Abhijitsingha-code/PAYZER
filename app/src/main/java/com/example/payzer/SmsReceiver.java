ca85f012c23ca31cb267b3bdbebb69841ed8191f
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-09-29 11:19:58 +0530

    Format the code and add another permission

--- app/src/main/java/com/example/payzer/SmsReceiver.java
+++ app/src/main/java/com/example/payzer/SmsReceiver.java
@@ -7,9 +7,10 @@ import android.telephony.SmsMessage;
 import android.util.Log;
 
 
-public abstract class SmsReciver extends BroadcastReceiver {
-    public void onRecive(Context context, Intent intent){
-        if(Telephony.Sms.Intents.SMS_REJECTED_ACTION.equals(intent.getAction())){
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+        if(Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())){
             for(SmsMessage SmaMsg: Telephony.Sms.Intents.getMessagesFromIntent(intent)){
                 String msg=SmaMsg.getMessageBody();
                 Log.e("TAG", "onRevive: " +msg);
